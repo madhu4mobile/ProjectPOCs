e4d1928835fd4b4794506c3a403ab36f6f4bf486
Madhu Muppala <madhu4mobile@gmail.com>
2021-09-28 18:30:14 -0500

    added more gatling scenarios

--- Stage5-mvn-karate-poc/karate-api/src/test/java/examples/externalAPIs/TestUsersSimulation.scala
+++ Stage5-mvn-karate-poc/karate-api/src/test/java/examples/externalAPIs/TestUsersSimulation.scala
@@ -2,13 +2,30 @@ package examples.externalAPIs
 
 import io.gatling.core.Predef._
 import com.intuit.karate.gatling.PreDef._
+
 import scala.concurrent.duration._
+import scala.language.postfixOps
 
 class TestUsersSimulation extends Simulation {
 
-  val getUser = scenario("getUsersCall").exec(karateFeature("classpath:examples/externalAPIs/reqResTestUser.feature"))
+  val getUser = scenario("get all users and then get the first user by id").exec(karateFeature("classpath:examples/externalAPIs/reqResUsers.feature"))
 
   setUp(
-    getUser.inject(rampUsers(1000) during(5 seconds))
+    getUser.inject(rampUsers(1000).during(5 seconds)
+  )
+  val testAUser = scenario("testAGivenUser").exec(karateFeature("classpath:examples/externalAPIs/reqResTestUser.feature"))
+
+  setUp(
+    getUser.inject(rampUsers(1000).during(5 seconds))
+  )
+
+  val create = scenario("createUser").exec(karateFeature("classpath:examples/users/users.feature")).exec(session => {
+    println("*** id in gatling: " + session("id").as[String])
+    println("*** session status in gatling: " + session.status)
+    session
+  }
+  )
+  setUp(
+    create.inject(rampUsers(200).during(5 seconds))
   )
 }
--- Stage5-mvn-karate-poc/karate-api/src/test/java/examples/users/users.feature
+++ Stage5-mvn-karate-poc/karate-api/src/test/java/examples/users/users.feature
@@ -1,3 +1,4 @@
+@ignore
 Feature: sample karate test script
   for help, see: https://github.com/intuit/karate/wiki/IDE-Support
 
